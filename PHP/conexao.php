23b624780b676358126d0fcd95c060ba7b61ad80
João <jmsmarchi@gmail.com>
2023-06-19 00:46:44 -0300

    ...

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,12 +1,101 @@
 <?php
+
+function conectaBD(){
     $usuario = 'root';
     $senha = '170205joao';
     $database = 'PPI';
     $host = 'localhost';
 
-    $mysqli = new mysqli($host, $usuario, $senha, $database);
+    $con=new PDO("mysql:host=$host;dbname=$database","$usuario","$senha");
 
-    if($mysqli->error){
-        die();
+    return $con;
+
+}
+
+function editarUsuario($id, $nome, $email,$senha, $novaData,$telefone){
+    try {
+    $con = conectaBD();
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "UPDATE usuarios SET nome=?, email=?, senha=?, data_nascimento=?, telefone=? WHERE id=?";
+    $stm = $con->prepare($sql);
+    $stm->bindParam(1, $nome);
+    $stm->bindParam(2, $email);
+    $stm->bindParam(3, $senha);
+    $stm->bindParam(4, $novaData);
+    $stm->bindParam(5, $telefone);
+    $stm->bindParam(6, $id);
+    $stm->execute();
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+}
+
+function insereUsuario($nome,$email,$senha){
+    try{
+    $con=conectaBD();
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql="INSERT INTO usuarios(nome,email,senha) VALUES (?,?,?)";
+    $stm=$con->prepare($sql);
+    $stm->bindParam(1,$nome);
+    $stm->bindParam(2,$email);
+    $stm->bindParam(3,$senha);
+    $stm->execute();
+    } catch(PDOException $e){
+        echo 'ERRO: '.$e->getMessage();
     }
+    return $con->lastInsertId();
+}
+
+
+function deletarUsuario($id){
+    $con=conectaBD();
+    $sql="DELETE FROM usuarios WHERE id=?";
+    try {
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stm=$con->prepare($sql);
+        $stm->bindParam(1,$id);
+        $stm->execute();
+    } catch (PDOException $e) {
+        echo 'ERRO:' .$e->getMessage();
+    }
+}
+
+    function recuperaUsuarioID($id){
+    $con=conectaBD();
+    $sql="SELECT * FROM usuarios WHERE id=?";
+    $stm=$con->prepare($sql);
+    $stm->bindParam(1,$id);
+    $stm->execute();
+    $result=$stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function recuperaAll(){
+    $con=conectaBD();
+    $sql="SELECT * FROM usuarios";
+    $stm=$con->prepare($sql);
+
+    $stm->execute();
+    $result=$stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function verificaLoginSenha($email, $senha) {
+    $con=conectaBD();
+   
+    $sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+    $stm= $con->prepare($sql);
+    $stm->bindParam(1,$email);
+    $stm->bindParam(2,$senha);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    
+   
+    if (count($result) > 0) {
+        header('Location: home.php');
+    } else {
+        echo "<script>alert('Usuario invalido, verifique suas credenciais ou inscreva-se!');</script>";
+    }
+}
+
 ?>
--- PHP/index.php
+++ PHP/index.php
@@ -6,12 +6,13 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    if(strlen($email) == 0){
-        echo "Preencha seu e-mail";
+    if(strlen($email) == 0 && strlen($senha) == 0){
+    }else if(strlen($email) == 0){
+        echo "<script>alert('Preencha seu e-mail');</script>";
     } else if(strlen($senha) == 0){
-        echo "Preencha sua senha";
+        echo "<script>alert('Preencha sua senha');</script>";
     }else{
-          echo "adsad";
+          verificaLoginSenha($email, $senha);
     }
 
 }
@@ -26,20 +27,65 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+<style>
+    body{
+        height: 98vh;
+        width: 98vw;
+        background-color: lightblue;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #botoes{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+    p{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #email{
+        margin-top: 25px;
+    }
+    #senha{
+        margin-bottom: 35px;
+    }
+    #form{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 450px;
+        height: 450px;
+        border: solid black 1px;
+    }
+    button{
+        background-color: yellow;
+        border-radius: 5%;
+        width: 100px;
+        height: 25px;
+        border: 0px;
+    }
+</style>
 <body>
-    <h1>ACESSE SUA CONTA</h1>
-    <form action="" method="POST">
-        <p>
-           <label for="email">EMAIL</label>  
-          <input type="text" name="email"> 
-        </p>
-        <p>
-           <label for="senha">Senha</label>  
-          <input type="password" name="senha"> 
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
+    <div id="form">
+        <h1>ACESSE SUA CONTA</h1>
+        <form action="" method="POST">
+            <p id="email">
+                <label for="email">EMAIL</label>  
+                <input type="text" name="email"> 
+            </p>
+            <p id="senha">
+                <label for="senha">Senha</label>  
+                <input type="password" name="senha"> 
+            </p>
+            <div id="botoes">
+                <button id="sign">Inscrever-se</button>
+                <button type="submit">Entrar</button>
+            </div>
+        </form>
+    </div> 
 </body>
 </html>
